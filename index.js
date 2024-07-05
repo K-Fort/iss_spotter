fd99e03724030df74c52ef7b31fe7599e7eabe27
K-Fort <fortink01@gmail.com>
2024-07-05 17:34:19 -0400

    fetchMyIp by coordinates implemented

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const  fetchMyIP  = require('./iss');
+const { fetchMyIP, fetchCoordsByIP } = require('./iss');
 
 fetchMyIP((error, ip) => {
   if (error) {
@@ -7,4 +7,14 @@ fetchMyIP((error, ip) => {
   }
 
   console.log('It worked! Returned IP:' , ip);
+
+  // Now call fetchCoordsByIP with the IP address
+  fetchCoordsByIP(ip, (error, data) => {
+    if (error) {
+      console.log("It didn't work!", error);
+      return;
+    }
+
+    console.log('It worked! Returned coordinates:', data);
+  });
 });
--- iss.js
+++ iss.js
@@ -1,13 +1,5 @@
 const needle = require('needle');
 
-/**
- * Makes a single API request to retrieve the user's IP address.
- * Input:
- *   - A callback (to pass back an error or the IP string)
- * Returns (via Callback):
- *   - An error, if any (nullable)
- *   - The IP address as a string (null if error). Example: "162.245.144.188"
- */
 const fetchMyIP = function(callback) {
   needle.get('https://api.ipify.org?format=json', (error, response, body) => {
     if (error) return callback(error, null);
@@ -17,9 +9,30 @@ const fetchMyIP = function(callback) {
       return;
     }
 
-    const ip = body.ip;
+    const ip = body.ip; // No need to call JSON.parse if body is already an object
     callback(null, ip);
   });
 };
 
-module.exports = fetchMyIP;
+const fetchCoordsByIP = function(ip, callback) {
+  needle.get(`http://ipwho.is/${ip}`, (error, response, body) => {
+
+    if (error) {
+      callback(error, null);
+      return;
+    }
+
+    if (!body.success) {
+      const message = `Success status was ${body.success}. Server message says: ${body.message} when fetching for IP ${body.ip}`;
+      callback(Error(message), null);
+      return;
+    } 
+
+    const latitude = body.latitude
+    const longitude = body.longitude
+    callback(null, {latitude, longitude});
+  });
+};
+
+
+module.exports = { fetchMyIP, fetchCoordsByIP };
